d2d0bed665e10d7d5d66b639e3838e44f4db86e5
Blake Ramsdell <blaker@gmail.com>
2011-05-12 00:30:56 -0700

    Fixed project target to find USB stuff, extracted accessory from Intent.

--- src/com/prototype_eng/hellofez/HelloFez.java
+++ src/com/prototype_eng/hellofez/HelloFez.java
@@ -2,11 +2,21 @@ package com.prototype_eng.hellofez;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.future.usb.UsbAccessory;
+import com.android.future.usb.UsbManager;
 
 public class HelloFez extends Activity {
+    private static final String TAG = "HelloFez";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreate");
+
+        UsbAccessory accessory = UsbManager.getAccessory(getIntent());
+        Log.v(TAG, (accessory != null) ? "accessory! I haz it!" : "No accessory for you!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
